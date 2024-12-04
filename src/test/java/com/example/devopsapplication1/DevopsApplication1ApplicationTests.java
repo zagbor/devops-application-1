a5c6ba51d48772bbfccb8bf6a18ea64680440b96
Boris Zagray <Boris.Zagray@x5.ru>
2024-12-04 21:10:48 +0200

    10

--- src/test/java/com/example/devopsapplication1/DevopsApplication1ApplicationTests.java
+++ src/test/java/com/example/devopsapplication1/DevopsApplication1ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.devopsapplication1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class DevopsApplication1ApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertEquals(1, 1);
     }
 
 }
